ac40320bb7938ef699ae3fa72c81d50420a14c18
sabbir <sabbir21122@gmail.com>
2022-04-18 23:41:48 +0600

    check box deleted

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -68,9 +68,7 @@ const Login = () => {
               <Form.Label>Password</Form.Label>
               <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
+            
             <p className='text-danger'>{errorInLogin?.message}</p>
             <Button type="submit" style={{ backgroundColor: "#00004d" }}>
               Login
